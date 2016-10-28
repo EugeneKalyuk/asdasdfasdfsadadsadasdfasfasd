f4676830132f0589aff800495677954ba61301d1
kalyuk_e <kalyuk@magora-systems.com>
2016-10-28 10:37:16 +0700

    refactoring

--- client/component/main/actions.js
+++
@@ -1,8 +0,0 @@
-export const MAIN_SET_SETTINGS = 'MAIN_SET_SETTINGS';
-
-export function setSettings(data) {
-  return {
-    type: MAIN_SET_SETTINGS,
-    payload: data
-  };
-}
---
+++ welcome.js
@@ -0,0 +1,5 @@
+export default function(message) {
+    alert(`Welcome ${message}`);
+    alert(process.env.USER)
+};
+
